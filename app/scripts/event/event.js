f19a35321daa768ee3b666fc79d46065f5f2f3eb
Konstantin Petrukhnov <konstantin.petrukhnov@gmail.com>
2014-03-15 04:29:43 +0200

    events (wip)

--- app/scripts/event/event.js
+++ app/scripts/event/event.js
@@ -2,7 +2,7 @@
  * Created by Konstantin Petrukhnov on 15.3.2014.
  */
 angular.module('someJamAppApp')
-  .controller('EventCtrl', ['$scope', '$http', function ($scope, $http) {
+  .controller('EventCtrl', ['$scope', '$http', '$modal', function ($scope, $http, $modal) {
     'use strict';
     $scope.dummy = dummy;
 
@@ -11,4 +11,50 @@ angular.module('someJamAppApp')
       {id: 2, interest: 'somejam', description: 'SomeJam is for people who are interested in service design, youth work, social work, social media and using a design-based approach to problem solving and creativity.', time: '16.03.2014 18:00', location: 'Youth Activity Center Happi', registered:39, interested: 120},
       {id: 3, interest: 'football', description: 'School football for girls. 10-12 years old.', time: '02.04.2014 11:00', location: 'Palomylly', registered:2, interested: 60}
     ];
+
+
+    var ModalInstanceCtrl = function ($scope, $modalInstance, event) {
+
+      $scope.event = event;
+
+      $scope.ok = function () {
+        $modalInstance.close(event);
+
+      };
+      $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+      };
+    };
+    $scope.newEvent = function () {
+      var modalInstance = $modal.open({
+        templateUrl: 'event/modal_edit.tpl.html',
+        controller: ModalInstanceCtrl,
+        backdrop: 'static',
+        resolve: {
+          event: function () {
+            return {};
+          }
+        }
+      });
+      modalInstance.result.then(function (newEvent) {
+        $scope.eventList.push(newEvent);
+      }, function () {
+      });
+    };
+    $scope.editEvent = function (event) {
+      var modalInstance = $modal.open({
+        templateUrl: 'event/modal_edit.tpl.html',
+        controller: ModalInstanceCtrl,
+        backdrop: 'static',
+        resolve: {
+          event: function () {
+            return angular.copy(event);
+          }
+        }
+      });
+      modalInstance.result.then(function (editedEvent) {
+        angular.copy(editedEvent, event);
+      }, function () {
+      });
+    };
   }]);
