6c6433eca622af3c0bc324d486e5775089eca819
Janne Vuorela <jannevv@gmail.com>
2014-03-15 21:53:16 +0200

    Improved eventDetails

--- app/scripts/controllers/eventdetails.js
+++ app/scripts/controllers/eventdetails.js
@@ -1,32 +1,48 @@
 'use strict';
 
 angular.module('someJamAppApp')
-  .controller('EventdetailsCtrl', function ($scope, $http, $routeParams) {
-      /*
+  .controller('EventdetailsCtrl', function ($scope, $http, $routeParams, UserSession) {
+      
       $scope.participants = "";
+      $scope.joinButtonStatus = 'Join';
+      
+      $scope.joinEvent = function(eventId) {
+          
+          if($scope.joinButtonStatus == 'Join') {
+              $http({ 
+                  method: 'POST',
+                  url: '/api/user/attend/event',
+                  data: {id:UserSession.id, eventId:eventId}
+              }).success(function(success) {
+                  $scope.joinButtonStatus = 'Unjoin';
+              });
+          } else {
+              // TODO: Unjoin
+          }
+              
+          
+      };
+
     
       $http.get('/api/event/id/'+$routeParams.id).success(function(data) {
           $scope.event = data;
-          //$date = data. 
           
           $http.get('/api/interest/id/'+data.interest).success(function(interest) {
               $scope.interest = interest.name;
           });
           
           var i = 0;
-          for(i=0; i<data.users.length; ++i) {
+          for(i=0; i<data.users.length; i++) {
               
               $http.get('/api/user/id/'+data.users[i]+'/username').success(function(user) {
                 
-                  $scope.participants = $scope.participants+user.username;
-                  if (i != data.users.length-1) {
-                      $scope.participants = $scope.participants+', ';
-                  }
+                  $scope.participants = $scope.participants+user.username+" ";
+                  
               });
           }
                                                                
             
-      });*/
+      });
       
         
         
